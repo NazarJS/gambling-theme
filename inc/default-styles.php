d2353d253405edb3e2030a8859d2a938760d24ee
yankazimirchick <yankazimirchick@gmail.com>
2023-06-12 15:38:03 +0300

    fox default file

--- inc/default-styles.php
+++ inc/default-styles.php
@@ -1,77 +1,71 @@
+<?php
+
+$custom_code_css = get_field('custom_code_css', 'options');
+$main_colors = get_field('main_colors', 'options');
+$header_colors = get_field('header_colors', 'options');
+$footer_colors = get_field('footer_colors', 'options');
+$links_color = get_field('link_colors', 'options');
+
+$first_header_button = get_field('first_header_button_color', 'options');
+$second_header_button = get_field('second_header_button_color', 'options');
+
+$first_header_button_arr = [
+    'color_bg' => $first_header_button['color_bg'] ?? '#ffffff',
+    'color_bg_hover' => $first_header_button['color_bg_hover'] ?? '#efefef',
+    'color_text' => $first_header_button['color_text'] ?? '#1b96f3',
+    'color_border' => $first_header_button['color_border'] ?? '#e3e3e3',
+    'color_border_hover' => $first_header_button['color_border_hover'] ?? '#efefef',
+];
+$second_header_button_arr = [
+    'color_bg' => $second_header_button['color_bg'] ?? '#ffffff',
+    'color_bg_hover' => $second_header_button['color_bg_hover'] ?? '#efefef',
+    'color_text' => $second_header_button['color_text'] ?? '#1b96f3',
+    'color_border' => $second_header_button['color_border'] ?? '#e3e3e3',
+    'color_border_hover' => $second_header_button['color_border_hover'] ?? '#efefef',
+];
+
+$main_colors_arr = [
+    'bg_body' => $main_colors['bg_body'] ?? '#ffffff',
+    'bg' => $main_colors['bg_color'] ?? '#efefef',
+    'accend_color' => $main_colors['accent__color'] ?? '#1b96f3',
+    'additional_color' => $main_colors['additional_color'] ?? '#e3e3e3',
+    'card_color' => $main_colors['card_color'] ?? '#efefef',
+    'text_color' => $main_colors['text_color'] ?? '#000000'
+];
+
+$header_colors_arr = [
+    'bg_color' => $header_colors['bg_color'] ?? '#1b96f3',
+    'text_color' => $header_colors['text_color'] ?? '#000',
+    'hover_nav_color' => $header_colors['hover_nav_color'] ?? '#1e73be',
+    'hover_nav_bg_color' => $header_colors['hover_nav_bg_color'] ?? '#1e73be',
+];
+$footer_colors = [
+    'bg_color' => $footer_colors['bg_color'] ?? '#1b96f3',
+    'text_color' => $footer_colors['text_color'] ?? '#fff',
+];
+
+$link_colors_arr = [
+    'link_color' => $links_color['link_color'] ?? '#1b96f3',
+    'link_color_hover' => $links_color['link_color_hover'] ?? '#000',
+];
+
+
+$buttons_bite = get_field('buttons_bite', 'options');
+
+$b_font_size = get_field('b_font_size', 'options') ?? '15';
+
+
+$bg_body = get_field('bg_body', 'options') ?? '#f8f8fa';
+$buttons_back = get_field('buttons_back', 'options');
+$buttons_border = get_field('buttons_border', 'options');
+$border_radius = get_field('border_radius', 'options') ?? '0';
+$review_bg_color = get_field('review_bg_color', 'options') ?? '#1d2730';
+$review_progress_bar = get_field('review_progress_bar', 'options') ?? '#e6b900';
+$review_text_progress_bar = get_field('review_text_progress_bar', 'options') ?? '#ffffff';
+
+?>
 <style>
 
-
-    <?php
-
-      $custom_code_css = get_field('custom_code_css', 'options');
-      $main_colors= get_field('main_colors', 'options');
-      $header_colors= get_field('header_colors', 'options');
-      $footer_colors= get_field('footer_colors', 'options');
-      $links_color = get_field('link_colors', 'options');
-
-      $first_header_button= get_field('first_header_button_color', 'options');
-      $second_header_button= get_field('second_header_button_color', 'options');
-
-      $first_header_button_arr=[
-        'color_bg' => $first_header_button['color_bg'] ?? '#ffffff',
-        'color_bg_hover' => $first_header_button['color_bg_hover'] ?? '#efefef',
-        'color_text' => $first_header_button['color_text'] ??'#1b96f3',
-        'color_border' => $first_header_button['color_border'] ?? '#e3e3e3',
-        'color_border_hover' => $first_header_button['color_border_hover'] ?? '#efefef',
-      ];
-       $second_header_button_arr=[
-        'color_bg' => $second_header_button['color_bg'] ?? '#ffffff',
-        'color_bg_hover' => $second_header_button['color_bg_hover'] ?? '#efefef',
-        'color_text' => $second_header_button['color_text'] ??'#1b96f3',
-        'color_border' => $second_header_button['color_border'] ?? '#e3e3e3',
-        'color_border_hover' => $second_header_button['color_border_hover'] ?? '#efefef',
-      ];
-
-      $main_colors_arr = [
-        'bg_body' => $main_colors['bg_body'] ?? '#ffffff',
-        'bg' => $main_colors['bg_color'] ?? '#efefef',
-        'accend_color' => $main_colors['accent__color'] ??'#1b96f3',
-        'additional_color' => $main_colors['additional_color'] ?? '#e3e3e3',
-        'card_color' => $main_colors['card_color'] ?? '#efefef',
-        'text_color' => $main_colors['text_color'] ?? '#000000'
-      ];
-
-      $header_colors_arr = [
-        'bg_color' => $header_colors['bg_color'] ?? '#1b96f3',
-        'text_color' => $header_colors['text_color'] ??'#000',
-        'hover_nav_color' => $header_colors['hover_nav_color'] ??'#1e73be',
-         'hover_nav_bg_color' => $header_colors['hover_nav_bg_color'] ??'#1e73be',
-      ];
-       $footer_colors = [
-        'bg_color' => $footer_colors['bg_color'] ?? '#1b96f3',
-        'text_color' => $footer_colors['text_color'] ??'#fff',
-      ];
-
-      $link_colors_arr = [
-        'link_color' => $links_color['link_color'] ?? '#1b96f3',
-        'link_color_hover' => $links_color['link_color_hover'] ??'#000',
-      ];
-
-
-      $buttons_bite= get_field('buttons_bite', 'options');
-
-      $b_font_size = get_field('b_font_size', 'options') ?? '15';
-
-
-
-
-      $bg_body = get_field('bg_body', 'options') ?? '#f8f8fa';
-      $buttons_back = get_field('buttons_back', 'options');
-      $buttons_border = get_field('buttons_border', 'options');
-      $border_radius = get_field('border_radius', 'options') ?? '0';
-      $review_bg_color = get_field('review_bg_color', 'options') ?? '#1d2730';
-      $review_progress_bar = get_field('review_progress_bar', 'options') ?? '#e6b900';
-      $review_text_progress_bar = get_field('review_text_progress_bar', 'options') ?? '#ffffff';
-
-
-
-
-    ?>
     :root {
         /* footer */
         --ft_background_color: <?= $footer_colors['bg_color']?>;
@@ -157,7 +151,3 @@ if (get_field('enable_google_fonts', 'options')) {
 
 <?php
 echo "<meta name='theme-color' content=" . $main_colors_arr['accend_color'] . ">";
-
-?>
-
-
